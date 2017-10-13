5f05298e26a9182693c690be51e5d348b4c99eba
Agustín Lorenzo Morales <agustin.lorenzi@gmail.com>
2017-10-13 16:47:06 +0200

    Helpers para control acceso listos

---
+++ common/models/PermissionHelpers.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace common\models;
+
+use common\models\ValueHelpers;
+use yii;
+use yii\web\Controller;
+use yii\helpers\Url;
+
+Class PermissionHelpers {
+	/**
+	 * check if the user is the owner of the record
+	 * use Yii::$app->user->identity->id for $userid, 'string' for model name
+	 * for example 'profile' will check the profile model to see if the user
+	 * owns the record. Provide the model instance, typically as $model->id as
+	 * the last parameter. Returns true or false, so you can wrap in if statement
+	 * @param mixed $userid
+	 * @param mixed $model_name
+	 * @param mixed $model_id
+	*/
+	public static function userMustBeOwner($model_name, $model_id) {
+		$connection = \Yii::$app->db;
+		$userid = Yii::$app->user->identity->id;
+		$sql = "SELECT id FROM $model_name WHERE user_id=:userid AND id=:model_id";
+		$command = $connection->createCommand($sql);
+		$command->bindValue(":userid", $userid);
+		$command->bindValue(":model_id", $model_id);
+		if ($result = $command->queryOne()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Método para requerir el tipo de usuario, si no está suscrito,
+	 * se le redirecciona a la página de suscripción.
+	 * @param mixed $user_type_name
+	 * @return \yii\web\Response
+	*/
+	public static function requireUpgradeTo($user_type_name)) {
+		if (Yii::$app->user->identity->user_type_id != 
+			ValueHelpers::getUserTypeValue($user_type_name)) {
+			return Yii::$app->getResponse()->redirect(Url::to(['suscripcion/index']));
+		}
+	}
+
+	/**
+	 * @requireStatus
+	 * @param mixed $status_name
+ 	*/
+ 	public static function requireStatus($status_name) {
+ 		if (Yii::$app->user_identity->status_id ==
+ 			ValueHelpers::getStatusValue($status_name)) {
+ 			return true;
+ 		} else {
+ 			return false;
+ 		}
+ 	}
+
+	/**
+	 * @requireMinStatus
+	 * @param mixed $status_name
+ 	*/
+ 	public static function requireMinStatus($status_name) {
+ 		if (Yii::$app->user_identity->status_id >=
+ 			ValueHelpers::getStatusValue($status_name)) {
+ 			return true;
+ 		} else {
+ 			return false;
+ 		}
+ 	}
+
+	/**
+	 * @requireRol
+	 * @param mixed $rol_name
+ 	*/
+ 	public static function requireRol($rol_name) {
+ 		if (Yii::$app->user_identity->rol_id ==
+ 			ValueHelpers::getRolValue($rol_name)) {
+ 			return true;
+ 		} else {
+ 			return false;
+ 		}
+ 	}
+
+	/**
+	 * @requireMinRol
+	 * @param mixed $rol_name
+ 	*/
+ 	public static function requireMinRol($rol_name) {
+ 		if (Yii::$app->user_identity->rol_id >=
+ 			ValueHelpers::getRolValue($rol_name)) {
+ 			return true;
+ 		} else {
+ 			return false;
+ 		}
+ 	}
+}
---
+++ common/models/ValueHelpers.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace common\models;
+
+Class ValueHelpers {
+
+	/**
+	 * Devuelve el valor de un nombre de rol
+	 * Por ejemplo: 
+	 * 		Recibe: "Admin"
+	 *		Devuelve: 20
+	 * @param mixed $rol_name
+	 * @return integer
+	*/
+
+	public static function getRolValue($rol_name) {
+		$connection = \Yii::$app->db;
+		$sql = "SELECT rol_value FROM roles WHERE rol_name = :rol_name";
+		$command = $connection->createCommand($sql);
+		$command = bindValue(":rol_name", $rol_name);
+		$result = $command->queryOne();
+
+		return $result['rol_value'];
+	}
+
+	/**
+	 * Devuelve el valor de un status
+	 * Por ejemplo: 
+	 * 		Recibe: "Pendiente"
+	 *		Devuelve: 5
+	 * @param mixed $status_name
+	 * @return integer
+	*/
+
+	public static function getStatusValue($status_name) {
+		$connection = \Yii::$app->db;
+		$sql = "SELECT status_value FROM status WHERE status_name = :status_name";
+		$command = $connection->createCommand($sql);
+		$command = bindValue(":status_name", $status_name);
+		$result = $command->queryOne();
+
+		return $result['status_value'];
+	}
+
+	/**
+	 * Devuelve el valor de un user_type_name para usar 
+	 * en métodos de PermissionHelpers
+	 * Por ejemplo: 
+	 * 		Recibe: "Suscrito"
+	 *		Devuelve: 30
+	 * @param mixed $user_type_name
+	 * @return integer
+	*/
+
+	public static function getUserTypeValue($user_type_name) {
+		$connection = \Yii::$app->db;
+		$sql = "SELECT user_type_value FROM user_type WHERE user_type_name = :user_type_name";
+		$command = $connection->createCommand($sql);
+		$command = bindValue(":user_type_name", $user_type_name);
+		$result = $command->queryOne();
+
+		return $result['user_type_value'];		
+	}
+
+}
