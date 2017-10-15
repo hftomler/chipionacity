6e2eb4b6a09d736c0955e998b4456c6227a35fc3
Agustín Lorenzo Morales <agustin.lorenzi@gmail.com>
2017-10-15 12:22:14 +0200

    Creado CRUD para User, Rol, Profile, UserType y Status

---
+++ backend/views/profile/index.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\search\ProfileSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = Yii::t('app', 'Profiles');
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="profile-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Create Profile'), ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'user_id',
+            'nombre',
+            'apellidos',
+            'gender_id',
+            // 'direccion',
+            // 'pais_id',
+            // 'municipio_id',
+            // 'cpostal',
+            // 'provincia_id',
+            // 'fecha_nac',
+            // 'created_at',
+            // 'updated_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
