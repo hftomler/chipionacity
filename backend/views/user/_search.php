bf47886a55e583da0e6c8176fc2deb60134328b7
Agustín Lorenzo Morales <agustin.lorenzi@gmail.com>
2017-10-18 16:35:17 +0200

    Pequeños ajustes responsive en userSearch

--- backend/views/user/_search.php
+++ backend/views/user/_search.php
@@ -15,20 +15,25 @@ use common\models\User;
         'action' => ['index'],
         'method' => 'get',
     ]); ?>
-
-    <?= $form->field($model, 'id') ?>
-
-    <?= $form->field($model, 'username') ?>
-
-    <?= $form->field($model, 'email') ?>
-
-    <?= $form->field($model, 'rol_id')->dropDownList(User::getrolList(), [ 'prompt' => 'Rol' ]);?>
-    <?= $form->field($model, 'user_type_id')->dropDownList(User::getuserTypeList(), [ 'prompt' => 'Tipo Usuario' ]);?>
-    <?= $form->field($model, 'status_id')->dropDownList($model->statusList, [ 'prompt' => 'Estado' ]);?>
-
-    <?= $form->field($model, 'proveedor')->checkbox() ?>
-
-    <div class="form-group">
+    <div class="col-xs-2 col-sm-3 col-lg-4">
+        <?= $form->field($model, 'id')?>
+    </div>
+    <div class="col-xs-10 col-sm-9 col-lg-8">
+        <?= $form->field($model, 'username')?>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+        <?= $form->field($model, 'email') ?>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+        <?= $form->field($model, 'rol_id')->dropDownList(User::getrolList(), [ 'prompt' => 'Rol' ]);?>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+        <?= $form->field($model, 'user_type_id')->dropDownList(User::getuserTypeList(), [ 'prompt' => 'Tipo Usuario' ]);?>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+        <?= $form->field($model, 'status_id')->dropDownList($model->statusList, [ 'prompt' => 'Estado' ]);?>
+    </div>
+    <div class="form-group col-xs-12">
         <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
         <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
     </div>
