657c5539a7b67fd4ad60d15b28528636816c93fa
Agustín Lorenzo Morales <agustin.lorenzi@gmail.com>
2017-10-22 23:27:40 +0200

    Profile update and create modificado paises, prov. y municip.

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -5,4 +5,16 @@ $(function(){
 			.find('#modalContent')
 			.load($(this).attr('value'));
 	});
+
+	$('#profile-pais_id').change(function(e) {
+		$.post( "index.php?r=profile/listaprov&id=" + $(this).val(), function ( data ) {
+			$( "select#profile-provincia_id" ).html( data );
+		});
+	});
+
+	$('#profile-provincia_id').change(function(e) {
+		$.post( "index.php?r=profile/listamuni&id=" + $(this).val(), function ( data ) {
+			$( "select#profile-municipio_id" ).html( data );
+		});
+	});
 });
