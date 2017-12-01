4baa693d4de02a6448a228ac6d01edfcdead81cf
Agustín Lorenzo Morales <agustin.lorenzi@gmail.com>
2017-12-01 23:01:26 +0100

    Mejoras en bloque de imágenes en update imágenes de servicio

--- backend/views/servicio/_form.php
+++ backend/views/servicio/_form.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use kartik\file\FileInput;
 use common\models\User;
+use yii\bootstrap\Collapse;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Servicios */
@@ -73,8 +74,33 @@ $isProv = User::isProveedor(Yii::$app->user->identity->id);
     <?php ActiveForm::end(); ?>
 
     <div class="col-xs-12 well">
-        <p class="tituloForms"><?= Yii::t('app', 'Actually images of the Service') ?></p>
-        <?= $model->getImagenServicio($model->id); ?>
+        <div class="col-xs-12 col-sm-6">
+            <?= $model->getImagenServicio($model->id); ?>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <?php echo Collapse::widget([
+                'class' => 'btn-primary',
+                'encodeLabels' => false,
+                'items' => [
+                    [
+                        'label' =>   '<i class="fa fa-info-circle tbn" aria-hidden="true"> ' . Yii::t('app', 'About Service Images') . '</i> ',
+                        'content' =>
+                            '<p>
+                                Las imágenes que se muestran a su izquierda son las que actualmente están guardadas y enlazadas con el servicio <span class="text-primary">
+                                ' . $model->descripcion . '</span>.<br />
+                                Al situarse sobre ellas, aparecerán dos iconos:<br/>
+                                     <i class="fa fa-eye text-success indented"></i> Este sirve para establecer la imagen como predeterminada, actualizando el campo updated_at.</br/>
+                                     <i class="fa fa-trash text-danger indented"></i> Este elimina la imagen de la base de datos y también del servidor, por lo que se borra completamente.</br/>
+                            </p>
+                            '
+                        // Descomentar lo siguiente si quiero que aparezca abierto por defecto
+                        //'contentOptions' => ['class' => 'in'],
+                    ],
+                ]
+            ]);
+
+            ?>
+        </div>
     </div>
 
 </div>
